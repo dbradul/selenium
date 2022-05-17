dcab5d2fbcab47c08f1011fc1db42e03e769ad45
Dmitriy Bradul <dbhost@mail.ru>
2022-05-17 23:01:04 +0300

    Switch to selenoid

--- main.py
+++ main.py
@@ -7,25 +7,41 @@ from selenium import webdriver
 
 def main():
 
-    options = webdriver.ChromeOptions()
-    # options.add_argument('window-size=1200x600')
-    # options.add_argument('disable-gpu')
-    # options.add_argument("remote-debugging-port=9223")
-    options.add_argument("disable-dev-shm-usage")
+    # options = webdriver.ChromeOptions()
+    # # options.add_argument('window-size=1200x600')
+    # # options.add_argument('disable-gpu')
+    # # options.add_argument("remote-debugging-port=9223")
+    # options.add_argument("disable-dev-shm-usage")
 
-    # options.add_argument("start-maximized")
+    # # options.add_argument("start-maximized")
 
+
+    # driver = webdriver.Remote(
+    #     command_executor='http://127.0.0.1:4444/wd/hub',
+    #     # desired_capabilities={
+    #     #     'browserName': 'chrome',
+    #     #     # 'javascriptEnabled': True
+    #     # },
+    #     options=options
+    # )
+        
+    capabilities = {
+        "browserName": "chrome",
+        "browserVersion": "latest",
+        "selenoid:options": {
+            "enableVNC": True,
+            "enableVideo": False
+        }
+    }
 
     driver = webdriver.Remote(
-        command_executor='http://127.0.0.1:4444/wd/hub',
-        # desired_capabilities={
-        #     'browserName': 'chrome',
-        #     # 'javascriptEnabled': True
-        # },
-        options=options
-    )
+        command_executor="http://selenoid:4444/wd/hub",
+        desired_capabilities=capabilities)
+
 
     driver.get('http://softwaretester.info/')
+
+    print()
 
 # def test_something(self):
 #     dt_format = '%Y%m%d_%H%M%S'
